28dab531825dc013dd12ccb814f57051a42273e7
Tanishka Kohli <tanishka.kohli00@gmail.com>
2021-06-09 22:12:14 +0530

    Teh with tim walkthrough #6 ending

--- inventory/src/App.js
+++ inventory/src/App.js
@@ -1,28 +1,24 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-import Info from "./info.js"
+// import { userState } from 'react';
+import SearchBar from "./SearchBar"
 
 function App() {
+   const [data,setData] = useState({});
+
+   const updateData = (seachParams) => {
+      setData(seachParams);
+   };
+
   return (
     <div>
-    <Info/>
-    <AddItem text = "Tanishka" number ={2}/>
+      <SearchBar callback = {updateData }></SearchBar>
+      <p>Name: {"name" in data ? data['name']: "No data to display"}</p>
+      <p>Price: {"price" in data ? data['price']: "No data to display"}</p>
+      <p>Type: {"type" in data ? data['type']: "No data to display"}</p>
+      <p>Brand: {"brand" in data ? data['brand']: "No data to display"}</p>
     </div>
-  );
-}
-
-
-
-function AddItem(props){
-  const value = props.text;
-  return(
-      <div>
-        <form>
-          <label for id = 'text-id'>Type Something: </label>
-          <input type = 'text' id = 'text-id' value={value}></input>
-        </form>
-      </div>
-
   );
 }
 
